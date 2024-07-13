8eb3244bc268bf0e6498899a9d04fddbfaeae517
Aashis Rijal <aashisrijal252@gmail.com>
2024-07-13 22:08:52 +0545

    order orderdetail and payment model code

---
+++ src/database/models/orderModel.ts
@@ -0,0 +1,55 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    CreatedAt
+} from 'sequelize-typescript';
+
+
+@Table({
+    tableName: "orders",
+    modelName: "Order",
+    timestamps: true
+})
+
+class Order extends Model{
+    @Column({
+        primaryKey: true,
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4
+    })
+    declare id:string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+
+    })
+    declare phoneNumber:string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+
+    })
+    declare shippingAddress:string;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false
+
+    })
+    declare totalAmount :number;
+
+    @Column({
+        type: DataType.ENUM('pending','cancelled','delivery','ontheway','preparation'),
+        defaultValue: 'pending'
+
+    })
+    declare orderStatus :string;
+
+    
+}
+
+export default Order;
